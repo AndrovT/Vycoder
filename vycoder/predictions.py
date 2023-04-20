07a632cd218c0ce322295a5fbe05cc22de341403
_ <_>
2023-04-20 13:13:30 +0200

    add different pair frequency model

--- vycoder/predictions.py
+++ vycoder/predictions.py
@@ -18,14 +18,29 @@ with open(data_path, newline="") as f:
 def uniform(x):
     return [1]*256
 
+
 def frequency(x):
     return frequencies
+
 
 def frequency_plus_uniform(alpha):
     return lambda x: [x+alpha*sum(frequencies)/256 for x in frequencies]
 
+
 def pair_frequency(alpha, beta):
     lookup = [[pairs[x][y]*sum(frequencies) + alpha*frequencies[y]*sum(pairs[x]) + sum(frequencies)*sum(pairs[x])*beta for y in range(256)] for x in range(257)]
+    assert all(max(row)/sum(row) < 0.5 for row in lookup)
+    def f(lst):
+        if len(lst):
+            x = lst[-1]
+        else:
+            x = 256
+        return lookup[x]
+    return f
+
+
+def pair_frequency2(alpha, beta):
+    lookup = [[pairs[x][y]*(sum(frequencies)+256*alpha) + beta*(frequencies[y] + alpha) for y in range(256)] for x in range(257)]
     assert all(max(row)/sum(row) < 0.5 for row in lookup)
     def f(lst):
         if len(lst):
